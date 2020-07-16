dae4ab2c3e9e8e5d12aab3b2755962f605b068d0
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-07-17 06:27:10 +0700

    a

---
+++ sdk/views/sdk.js
@@ -0,0 +1,30 @@
+﻿var IO = (function () {
+    function UIEngine(options) {
+        options = options || {};
+        var _ID = new Date().getTime();
+
+        var _vue = {
+            app: null,
+            temps = {},
+            coms =[],
+            regType = {},
+            mixin = {}
+        };
+
+        var _app = {
+            init: function () { }
+        }
+
+        return {
+            init: _app.init(),
+            Vue: {
+                App: _vue.app,
+                Mixin: _vue.mixin
+            }
+        }
+    }
+
+    var instance;
+    return { getInstance: function (options) { if (instance === undefined) { instance = new UIEngine(options); } return instance; } };
+})().getInstance();
+IO.init();
