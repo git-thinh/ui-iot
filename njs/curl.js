5faaabd78c627520c4bbbb3b9d529f4fcadfe2c1
thinhmascot <67301649+thinhmascot@users.noreply.github.com>
2020-08-06 17:52:36 +0700

    TEST

--- njs/curl.js
+++ njs/curl.js
@@ -1,24 +1,42 @@
-﻿var PORT = process.env.PORT;
+﻿const PORT = process.env.PORT;
+//const PORT = 12345;
+
+const _ = require('lodash');
 const FETCH = require('node-fetch');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const HTTP = require('http');
 const URL = require('url');
 const QUERY_STRING = require('querystring');
+const CACHES = [];
+
 const SERVER = HTTP.createServer(function (req, res) {
     const uri = URL.parse(req.url);
     const query = QUERY_STRING.parse(uri.query);
-
-    if (query && query.url) {
-        FETCH(query.url).then(r => r.text()).then(text => {
-            //console.log(s);
+    var link = query.url;
+    
+    if (query && link) {
+        var htm = _.find(CACHES, function (o) { return o.Url == link; });
+        //console.log(htm == null, link);
+        if (htm) {
             res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-            res.write(text, "utf-8");
+            res.write(htm.Html, "utf-8");
             res.end();
-        });
+        } else {
+            FETCH(link).then(r => r.text()).then(text => {
+                CACHES.push({ Url: link, Html: text });
+                //console.log(s);
+                res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                res.write(text, "utf-8");
+                res.end();
+            });
+        }
     } else {
         res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
         res.write("", "utf-8");
         res.end();
     }
 });
-SERVER.listen(PORT);
+
+SERVER.listen(PORT, function () {
+    console.log(PORT);
+});
--- njs/theme.js
+++ njs/theme.js
@@ -1,48 +1,56 @@
-var PORT = process.env.PORT;
+//const PORT = process.env.PORT;
+const PORT = 3456;
+
+const puppeteer = require('puppeteer');
+const _ = require('lodash');
 const FETCH = require('node-fetch');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const HTTP = require('http');
 const URL = require('url');
 const QUERY_STRING = require('querystring');
-const SERVER = HTTP.createServer(function (req, res) {
+const CACHES = [];
+
+const SERVER = HTTP.createServer(async function (req, res) {
     const uri = URL.parse(req.url);
     const query = QUERY_STRING.parse(uri.query);
+    var link = query.url;
 
-    if (query && query.url) {
-        FETCH(query.url).then(r => r.text()).then(text => {
+    if (query && link) {
+        var htm = _.find(CACHES, function (o) { return o.Url == link; });
+        //console.log(htm == null, link);
+        if (htm) {
+            res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+            res.write(htm.Html, "utf-8");
+            res.end();
+        } else {
+            var arr = [];
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.on('response', async (response) => {
+                var url_ = response.url();
+                var header = response.headers();
+                var type = header['content-type'];
+                //console.log(await response.json());
+                //console.log(type + ' ' + url_);
+                arr.push(type + '|' + url_);
+            });
+
+            await page.goto(link);
+            await browser.close();
+
+            CACHES.push({ Url: link, Links: arr });
             //console.log(s);
             res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
             res.write(text, "utf-8");
             res.end();
-        });
+        }
     } else {
         res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
         res.write("", "utf-8");
         res.end();
     }
 });
-SERVER.listen(PORT);
 
-
-function test(text) {
-    FETCH('https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000').then(r => r.text()).then(s => {
-        console.log(s);
-    })
-}
-
-const READ_LINE = require("readline");
-const RL = READ_LINE.createInterface({ input: process.stdin, output: process.stdout });
-RL.on("line", function (line) {
-    let text = line.trim();
-    switch (text) {
-        case 'exit':
-            process.exit();
-            break;
-        case 'cls':
-            console.clear();
-            break;
-        default:
-            test(text);
-            break;
-    }
+SERVER.listen(PORT, function () {
+    console.log(PORT);
 });
